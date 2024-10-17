64d7d1fb72185a005960f5eb6e1da08bc41630de
Mark Peters <mapeters101@earthlink.net>
2024-10-17 00:37:02 -0400

    Lesson008: Complete work with class attributes from 4:46.

--- classAttributes.py
+++ classAttributes.py
@@ -26,8 +26,30 @@ print(getattr(Program, 'x', 'N/A'))
 
 print(getattr('hello', 'x', 'N/A'))
 
+Program.x = 100
+print(Program.x, getattr(Program, 'x'))
 
+a = 'hello'
+#a.x = 100
+#str.x = 100
 
+print(Program.__dict__)
 
+setattr(Program, 'x', -100)
+print(Program.__dict__)
 
+delattr(Program, 'x')
+print(Program.__dict__)
+
+Program.y = 'hello'
+print(Program.__dict__)
+del Program.y
+print(Program.__dict__)
+
+print(Program.__dict__['language'])
+print(list(Program.__dict__.items()))
+
+#Program.__dict__['language'] = 'Java'
+
+print(Program.__name__)
 
